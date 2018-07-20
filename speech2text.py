a5a16cb594bcd5fa813bc3d75283a8adb237dce1
fangminjie <fangminjie@bytedance.com>
2018-07-20 18:57:36 +0800

    Added script for transcribing speech to text.
    Change-Id: Id2460b44928d49ae91aa3107dfead637253c0f3d

---
+++ speech2text.py
@@ -0,0 +1,42 @@
+from aip import AipSpeech
+import pandas as pd
+import os
+import sys
+
+""" 你的 APPID AK SK """
+APP_ID = '11543212'
+API_KEY = 'jmmOKK8Gkzv6UMTa3nNR7wfv'
+SECRET_KEY = 'HCkGzPGEY1d32HZ50eApvnTkd6X53qqN'
+
+client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
+text = pd.DataFrame(columns=('Language', 'Gender', 'Filename', 'Text'))
+
+
+def get_file_content(filePath):
+    with open(filePath, 'rb') as fp:
+        return fp.read()
+
+
+def speech2text(language, gender):
+    global text
+    dir_path = 'audio/segment/%s/%s/' % (language, gender)
+    filenames = os.listdir(dir_path)
+    for i, filename in enumerate(filenames):
+        sys.stdout.write('%d/%d\r' % (i, len(filenames)))
+        sys.stdout.flush()
+        re = client.asr(get_file_content(dir_path + filename), 'wav', 16000, {'dev_pid': 1536 if language == 'cn' else 1737})
+        if re['err_no'] == 0:
+            text = text.append({'Language': language, 'Gender': gender,
+                                'Filename': filename, 'Text': re['result']}, ignore_index=True)
+        else:
+            print(re)
+
+try:
+    speech2text('cn', 'male')
+    speech2text('cn', 'female')
+    speech2text('en', 'male')
+    speech2text('en', 'female')
+except BaseException as e:
+    print(e)
+finally:
+    text.to_csv('text/recognized_text.csv')
