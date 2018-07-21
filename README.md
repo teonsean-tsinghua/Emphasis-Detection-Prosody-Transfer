275397ebc1327c60e93e015e765bf842d6510521
fangminjie <fangminjie@bytedance.com>
2018-07-21 10:19:04 +0800

    Implemented parallelization of speech and text.
    Change-Id: I1eb1123d096490b0c5c19166e6e9fe21a6813adc

--- README.md
+++ README.md
@@ -9,7 +9,10 @@
 * pydub
 * pandas
 ## Steps
+### Data Retrieval
 1. （optional）分别运行presegmentation.py中两段语句，在text/目录下会生成两个csv文件。该步骤耗时较长，因此csv文件将上传至repo中。
 2. 运行segmentation.py,将cn.wav和en.wav两个原始音轨中的男声片段与女声片段切分出来，分别存储在audio/segment/cn(en)/male(female)目录下。
 3. （optional）运行transcription.py，调用百度的语音识别api，将切分后的语音转录为文字，以csv格式存放在text文件夹下。四个csv文件同样上传至repo中。
-4. 挑选可用数据，利用selection.py脚本生成描述可用数据集的csv文件text/dataset_meta.csv.
+4. 挑选可用数据，利用selection.py脚本生成描述可用数据集的csv文件text/dataset_meta.csv。
+### Data Processing
+1. 运行parallelization.py，将dataset_meta.csv中描述的所有语音与文字进行时域上的对齐，生成text/dataset_time_align.csv。
--- parallelization.py
+++ parallelization.py
@@ -1,11 +1,100 @@
 import os
 import pandas as pd
+import subprocess
+import xml.etree.ElementTree as et
+
 
 try:
     os.mkdir('tmp')
 except FileExistsError:
     pass
 
-dataset = pd.read_csv('text/dataset_meta.csv', header=0)
-for _, row in dataset.iterrows():
-    os.system('echo "%s"' % row['CnText'])
+path = os.popen('pwd').readlines()[0].strip()
+meta = pd.read_csv('text/dataset_meta.csv', header=0)
+align = pd.DataFrame(columns=('Language', 'Gender', 'Filename', 'Active time', 'Alignment'))
+
+
+def translate(lang, gend, name):
+    global align
+    tree = et.parse('./tmp/palign.xra')
+    doc = tree.getroot()
+    active_time = []
+    alignment = []
+    for child in doc:
+        if child.tag != 'Tier':
+            continue
+        if child.attrib['tiername'] == 'Activity':
+            for annotation in child:
+                if annotation.find('Label').find('Tag').text == 'speech':
+                    interval = annotation.find('Location').find('Interval')
+                    begin = int(float(interval.find('Begin').get('midpoint')) * 1000)
+                    end = int(float(interval.find('End').get('midpoint')) * 1000)
+                    active_time.append((begin, end))
+        elif child.attrib['tiername'] == 'TokensAlign':
+            for annotation in child:
+                tag = annotation.find('Label').find('Tag').text
+                if tag == '#':
+                    continue
+                interval = annotation.find('Location').find('Interval')
+                begin = int(float(interval.find('Begin').get('midpoint')) * 1000)
+                end = int(float(interval.find('End').get('midpoint')) * 1000)
+                alignment.append((tag, begin, end))
+    align = align.append({'Language': lang, 'Gender': gend, 'Filename': name,
+                  'Active time': active_time, 'Alignment': alignment}, ignore_index=True)
+
+
+for i, row in meta.iterrows():
+    print('\n\n\n=================================================\nprocessing %d/%d examples.' % (i+1, len(meta)))
+    commands = []
+    commands.append('cd %s' % path)
+    commands.append('echo %s | ' % row['CnText'] +
+                    'python ./SPPAS/sppas/bin/normalize.py ' +
+                    '-r ./SPPAS/resources/vocab/cmn.vocab ' +
+                    '> ./tmp/text.txt')
+    commands.append('python ./SPPAS/sppas/bin/wavsplit.py ' +
+                    '-w ./audio/segment/cn/%s/%s ' % (row['Gender'], row['CnFile']) +
+                    '-t ./tmp/text.txt ' +
+                    '-p ./tmp/segm.xra')
+    commands.append('python ./SPPAS/sppas/bin/normalize.py ' +
+                    '-i ./tmp/segm.xra ' +
+                    '-r ./SPPAS/resources/vocab/cmn.vocab ' +
+                    '-o ./tmp/norm.xra')
+    commands.append('python ./SPPAS/sppas/bin/phonetize.py ' +
+                    '-i ./tmp/norm.xra ' +
+                    '-r ./SPPAS/resources/dict/cmn.dict ' +
+                    '-o ./tmp/phon.xra')
+    commands.append('python ./SPPAS/sppas/bin/alignment.py ' +
+                    '-w ./audio/segment/cn/%s/%s ' % (row['Gender'], row['CnFile']) +
+                    '-i ./tmp/phon.xra ' +
+                    '-I ./tmp/norm.xra ' +
+                    '-r ./SPPAS/resources/models/models-cmn ' +
+                    '-o ./tmp/palign.xra')
+    subprocess.call(' && '.join(commands), shell=True)
+    translate('cn', row['Gender'], row['CnFile'])
+    commands = []
+    commands.append('cd %s' % path)
+    commands.append('echo %s | ' % row['EnText'] +
+                    'python ./SPPAS/sppas/bin/normalize.py ' +
+                    '-r ./SPPAS/resources/vocab/eng.vocab ' +
+                    '> ./tmp/text.txt')
+    commands.append('python ./SPPAS/sppas/bin/wavsplit.py ' +
+                    '-w ./audio/segment/en/%s/%s ' % (row['Gender'], row['EnFile']) +
+                    '-t ./tmp/text.txt ' +
+                    '-p ./tmp/segm.xra')
+    commands.append('python ./SPPAS/sppas/bin/normalize.py ' +
+                    '-i ./tmp/segm.xra ' +
+                    '-r ./SPPAS/resources/vocab/eng.vocab ' +
+                    '-o ./tmp/norm.xra')
+    commands.append('python ./SPPAS/sppas/bin/phonetize.py ' +
+                    '-i ./tmp/norm.xra ' +
+                    '-r ./SPPAS/resources/dict/eng.dict ' +
+                    '-o ./tmp/phon.xra')
+    commands.append('python ./SPPAS/sppas/bin/alignment.py ' +
+                    '-w ./audio/segment/en/%s/%s ' % (row['Gender'], row['EnFile']) +
+                    '-i ./tmp/phon.xra ' +
+                    '-I ./tmp/norm.xra ' +
+                    '-r ./SPPAS/resources/models/models-eng ' +
+                    '-o ./tmp/palign.xra')
+    subprocess.call(' && '.join(commands), shell=True)
+    translate('en', row['Gender'], row['EnFile'])
+align.to_csv('./text/dataset_time_align.csv', index=False)
